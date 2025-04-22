d17443f327eb0ef399d4dd1058ff92ec19e45323
VashRogers <rogerssj2@gmail.com>
2025-04-21 21:47:37 -0400

    adicionando regras do prettier e começando a configurar router

---
+++ src/router/index.jsx
@@ -0,0 +1,5 @@
+import { SystemRoutes } from "./system-routes";
+
+export default function Router() {
+    return <SystemRoutes />;
+}
---
+++ src/router/system-routes.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Routes } from "react-router"
+
+export function SystemRoutes() {
+    return(
+        <Routes>
+
+        </Routes>
+    )
+}
